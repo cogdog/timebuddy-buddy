db0070ec1d2099549d70dfd2cf92064ff0f8eda9
CogDog <cogdogblog@gmail.com>
2023-06-22 09:14:46 -0600

    Shortcode checks

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Wordpress Plugin for World Time Buddy Events
 
 Write me later.
+
+Hey, It's later, and nothing here,
+
+Coming soon.
+
+Updated to provide shortcode version that checks params for start with "h=" and also includes a "md=".
--- timebuddy-buddy.php
+++ timebuddy-buddy.php
@@ -2,7 +2,7 @@
 /*
 * Plugin Name: Time Buddy Buddy
 * Description: Shortcode and editor button for adding World Time Buddy Event Widgets
-* Version: 0.2
+* Version: 0.3
 * Author: @cogdog
 * Author URI: https://cog.dog
 
@@ -18,13 +18,17 @@ function do_vc_timebuddy ( $atts ) {
  	extract( shortcode_atts( array( "params" => ""), $atts ) );  
  	
  	if ( empty( $params ) ) {
- 		return "Missing parameter! set <code>params=\"xxxxx\" from the Timebuddy generated embed code for <code>src=\"</code> e.g. <code>params=\"h=2643743&md=2/24/2017&mt=19.50&ml=1.00&sts=0&sln=0&wt=ew -ltc\"</code> after the \"?\"";
+ 		return "<p><strong>Missing parameter for Timebuddy widget!</strong> -- set <code>params=\"xxxxx\"</code> from the Timebuddy generated embed code for <code>src=\"</code> e.g. <code>params=\"h=2643743&md=2/24/2023&mt=19.50&ml=1.00&sts=0&sln=0&wt=ew -ltc\"</code> after the \"?\"</p>";
+ 		
+ 	} elseif ( substr($params,0,2) !='h=' or strpos($params, 'md') ===false) {	
+ 	return "<p><strong>Malformed parameter! for Timebuddy widget!</strong> <code>params=" . $params . "</code> is not correct. check <code>params=\"xxxxx\"</code>  from the Timebuddy generated embed code for <code>src=\"</code> e.g. <code>params=\"h=2643743&md=4/27/2020&mt=19.50&ml=1.00&sts=0&sln=0&wt=ew -ltc\"</code> after the \"?\"</p>";
+ 		
  	} else {
  	
  		// if code entered does now have code for countdown clock, add one
  		if ( substr( $params, -1 ) != 'c' ) $params .= 'c';
  
- 		return '<div align="center"><span class="wtb-ew-v1" style="width: 560px; display:inline-block"><script src="https://www.worldtimebuddy.com/event_widget.js?' . $params . '"></script><i><a target="_blank" href="https://www.worldtimebuddy.com/">Time converter</a> at worldtimebuddy.com</i><noscript><a href="https://www.worldtimebuddy.com/">Time converter</a> at worldtimebuddy.com</noscript><script>window[wtb_event_widgets.pop()].init()</script></span></div>';
+ 		return '<div style="text-align:center"><span class="wtb-ew-v1" style="width: 560px; display:inline-block"><script src="https://www.worldtimebuddy.com/event_widget.js?' . $params . '"></script><i><a target="_blank" href="https://www.worldtimebuddy.com/">Time converter</a> at worldtimebuddy.com</i><noscript><a href="https://www.worldtimebuddy.com/">Time converter</a> at worldtimebuddy.com</noscript><script>window[wtb_event_widgets.pop()].init()</script></span></div>';
 	
 	}
 
